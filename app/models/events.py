8c4e31f41ed2b7033a5cc129505eb443403a7f8b
ronniegikandi <kiiruronnie42@gmil.com>
2026-01-15 08:22:42 +0300

    creating models/event.py

---
+++ app/models/events.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..database import Base
+
+class Event(Base):
+    
