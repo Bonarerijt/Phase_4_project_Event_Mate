97aeb4db80824faca7a55c8ef7dc4a2df670884a
Bonarerijt <prudyjj4@gmail.com>
2026-01-15 14:29:12 +0300

    Adding Environment configurations

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,9 @@
-# TODO: Implement
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    
