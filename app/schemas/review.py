d01f6bdff3316436340bb1c46f8c5c2dad54e486
ronniegikandi <kiiruronnie42@gmil.com>
2026-01-15 08:37:00 +0300

    working on schemas/participation

--- app/schemas/review.py
+++ app/schemas/review.py
@@ -4,3 +4,14 @@ class ReviewCreate(BaseModel):
     event_id: int
     rating: int
     comment: str
+
+
+class ReviewResponse(BaseModel):
+    id: int
+    event_id: int
+    user_id: int
+    rating: int
+    comment: str
+
+    class Config:
+        from_attributes = True
