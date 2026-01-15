a2b1a22fb95d1589585cb394abf119457db37948
ronniegikandi <kiiruronnie42@gmil.com>
2026-01-15 08:32:02 +0300

    working on schemas/event.py...

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,3 +1,11 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
+
+
+class EventBase(BaseModel):
+    title: str
+    description: str
+    location: str
+    datetime: datetime
+    organizer_id: int
