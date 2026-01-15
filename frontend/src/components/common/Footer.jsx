547c8ffd3c0be0bd9c2a4f82803a458863673f78
Kenneth-Kipkosgei <kennethkipkosgei38@gmail.com>
2026-01-15 14:30:30 +0300

    Add Footer component

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,1 +1,9 @@
-// TODO: Implement
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 EventMate. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
