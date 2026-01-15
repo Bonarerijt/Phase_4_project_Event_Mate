39062b74b519ba37efefe585bdf72f8340bf9936
Bonarerijt <prudyjj4@gmail.com>
2026-01-15 14:39:01 +0300

    Created instance of my FastAPI application

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,4 +5,12 @@ from .routers import auth_router, events_router, participation_router, reviews_r
 
 Base.metadata.create_all(bind=engine)
 
+app = FastAPI(title="EventMate API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
