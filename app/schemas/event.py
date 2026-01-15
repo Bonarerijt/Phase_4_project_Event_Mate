34435f8adefcfab3e274822e6f4c708dcc066408
ronniegikandi <kiiruronnie42@gmil.com>
2026-01-15 08:33:06 +0300

    proving eventactions in the schemas and creating its logic

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -9,3 +9,17 @@ class EventBase(BaseModel):
     location: str
     datetime: datetime
     organizer_id: int
+
+
+class EventCreate(EventBase):
+    pass
+class EventUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    location: Optional[str] = None
+    datetime: Optional[datetime] = None
+class EventInDB(EventBase):
+    id: int
+
+    class Config:
+        orm_mode = True
